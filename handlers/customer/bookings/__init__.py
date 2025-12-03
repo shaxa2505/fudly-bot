9462f942dbb5e3a30b03d22cb8d5172f133d16b0
Shaxa2505 <noisyes321@gmail.com>
2025-12-04 02:27:17 +0500

    Cleanup: remove dead code and empty directories

--- handlers/customer/bookings/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Customer bookings handlers - create, view, manage bookings."""
-
-from .router import router as bookings_router
-
-__all__ = ["bookings_router"]
--- handlers/customer/bookings/router.py
+++
@@ -1,6 +0,0 @@
-"""
-Customer bookings router - create, view bookings.
-"""
-from aiogram import Router
-
-router = Router(name='customer_bookings')
