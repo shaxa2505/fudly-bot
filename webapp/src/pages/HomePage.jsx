1aba6b97f3806c049a4b5aa3f4676fb6c49928ea
shaxa2505 <noisyes321@gmail.com>
2026-01-22 18:51:17 +0500

    a

--- webapp/src/pages/HomePage.jsx
+++ webapp/src/pages/HomePage.jsx
@@ -931,7 +931,7 @@ function HomePage() {
       <header className="header">
         <div className="header-top">
           <div className="header-location-block">
-            <span className="header-location-label">Joylashuv</span>
+            <span className="header-location-label">Manzil</span>
             <button className="header-location" onClick={openAddressModal}>
               <span className="header-location-city-name">{cityLabel}</span>
               <svg className="header-location-caret" width="14" height="14" viewBox="0 0 24 24" fill="none" aria-hidden="true">
@@ -939,12 +939,6 @@ function HomePage() {
               </svg>
             </button>
           </div>
-          <button className="header-notification" type="button" aria-label="Bildirishnomalar">
-            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" aria-hidden="true">
-              <path d="M15 17h5l-1.4-1.4A2 2 0 0118 14.2V11a6 6 0 10-12 0v3.2a2 2 0 01-.6 1.4L4 17h5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-              <path d="M9 17a3 3 0 006 0" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-          </button>
         </div>
       </header>
 
